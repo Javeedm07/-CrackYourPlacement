e3fec0aa17a66b7a7d20a003aba79d4e5339fb23
Javeedm07 <114037880+Javeedm07@users.noreply.github.com>
2024-07-29 22:02:36 +0530

    Update Add_Binary(67).py

--- Day_4/Add_Binary(67).py
+++ Day_4/Add_Binary(67).py
@@ -6,3 +6,29 @@ string using the `bin()` function, and the `[2:]` slice is used to remove the '0
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return (bin(int(a, 2) + int(b, 2)))[2:]
+
+"""
+Intuition:
+The code defines helper functions to convert binary strings to integers (`to_int`) and integers to binary strings (`to_bin`). The `addBinary` function uses 
+these helpers to convert binary strings to integers, add them, and convert the result back to a binary string.
+"""
+class Solution:
+    def to_int(self, n):
+        # n = str(n)
+        ans = 0
+        for i in range(len(n)):
+            ans += int(n[len(n) - 1 - i]) * (2 ** i)
+        return ans
+
+    def to_bin(self, n):
+        if n == 0:
+            return "0"
+        ans = ""
+        while n > 0:
+            ans = str(n % 2) + ans
+            n = n // 2
+        return ans
+
+    def addBinary(self, a: str, b: str) -> str:
+        ans = self.to_int(a) + self.to_int(b)
+        return self.to_bin(ans)
